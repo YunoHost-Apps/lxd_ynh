4bd7be5626c8d8552619f42fa034056ac3c97c58
Kay0u <pierre@kayou.io>
2021-03-01 19:00:42 +0100

    First version

--- README.md
+++ README.md
@@ -1,43 +1,27 @@
-# Usage of this package (REMOVE THIS SECTION BEFORE RELEASE)
-- Copy this app before working on it.
-- Edit the `conf/nginx.conf` file to match app prerequisites.
-- Edit the `manifest.json` with app specific info.
-- Edit the `install`, `upgrade`, `remove`, `backup`, and `restore` scripts.
-  - Using the [script helpers documentation.](https://yunohost.org/#/packaging_apps_helpers)
-- Add a `LICENSE` file for the package.
-- Edit `README.md` and `README_fr.md`.
+# LXD for YunoHost
 
-# Example app for YunoHost
+[![Integration level](https://dash.yunohost.org/integration/lxd.svg)](https://dash.yunohost.org/appci/app/lxd) ![](https://ci-apps.yunohost.org/ci/badges/lxd.status.svg) ![](https://ci-apps.yunohost.org/ci/badges/lxd.maintain.svg)  
+[![Install lxd with YunoHost](https://install-app.yunohost.org/install-with-yunohost.svg)](https://install-app.yunohost.org/?app=lxd)
 
-[![Integration level](https://dash.yunohost.org/integration/REPLACEBYYOURAPP.svg)](https://dash.yunohost.org/appci/app/REPLACEBYYOURAPP) ![](https://ci-apps.yunohost.org/ci/badges/REPLACEBYYOURAPP.status.svg) ![](https://ci-apps.yunohost.org/ci/badges/REPLACEBYYOURAPP.maintain.svg)  
-[![Install REPLACEBYYOURAPP with YunoHost](https://install-app.yunohost.org/install-with-yunohost.svg)](https://install-app.yunohost.org/?app=REPLACEBYYOURAPP)
-
-*[Lire ce readme en français.](./README_fr.md)*
-
-> *This package allows you to install REPLACEBYYOURAPP quickly and simply on a YunoHost server.  
+> *This package allows you to install lxd quickly and simply on a YunoHost server.  
 If you don't have YunoHost, please consult [the guide](https://yunohost.org/#/install) to learn how to install it.*
 
 ## Overview
-Quick description of this app.
+LXD is a next generation system container manager. It offers a user experience similar to virtual machines but using Linux containers instead.
 
-**Shipped version:** 1.0
-
-## Screenshots
-
-![](Link to a screenshot of this app.)
+**Shipped version:** 4.11
 
 ## Demo
 
-* [Official demo](Link to a demo site for this app.)
+* [Official demo](https://linuxcontainers.org/lxd/try-it/)
 
 ## Configuration
 
-How to configure this app: From an admin panel, a plain file with SSH, or any other way.
+How to configure this app: In cli
 
 ## Documentation
 
- * Official documentation: Link to the official documentation of this app
- * YunoHost documentation: If specific documentation is needed, feel free to contribute.
+ * Official documentation: https://linuxcontainers.org/lxd/
 
 ## YunoHost specific features
 
@@ -48,8 +32,8 @@ Can the app be used by multiple users?
 
 #### Supported architectures
 
-* x86-64 - [![Build Status](https://ci-apps.yunohost.org/ci/logs/REPLACEBYYOURAPP%20%28Apps%29.svg)](https://ci-apps.yunohost.org/ci/apps/REPLACEBYYOURAPP/)
-* ARMv8-A - [![Build Status](https://ci-apps-arm.yunohost.org/ci/logs/REPLACEBYYOURAPP%20%28Apps%29.svg)](https://ci-apps-arm.yunohost.org/ci/apps/REPLACEBYYOURAPP/)
+* x86-64 - [![Build Status](https://ci-apps.yunohost.org/ci/logs/lxd%20%28Apps%29.svg)](https://ci-apps.yunohost.org/ci/apps/lxd/)
+* ARMv8-A - [![Build Status](https://ci-apps-arm.yunohost.org/ci/logs/lxd%20%28Apps%29.svg)](https://ci-apps-arm.yunohost.org/ci/apps/lxd/)
 
 ## Limitations
 
@@ -64,9 +48,9 @@ https://yunohost.org/packaging_apps
 
 ## Links
 
- * Report a bug: https://github.com/YunoHost-Apps/REPLACEBYYOURAPP_ynh/issues
- * App website: Link to the official website of this app.
- * Upstream app repository: Link to the official repository of the upstream app.
+ * Report a bug: https://github.com/YunoHost-Apps/lxd_ynh/issues
+ * App website:  https://linuxcontainers.org
+ * Upstream app repository: https://github.com/lxc/lxd
  * YunoHost website: https://yunohost.org/
 
 ---
@@ -74,11 +58,11 @@ https://yunohost.org/packaging_apps
 ## Developer info
 
 **Only if you want to use a testing branch for coding, instead of merging directly into master.**
-Please send your pull request to the [testing branch](https://github.com/YunoHost-Apps/REPLACEBYYOURAPP_ynh/tree/testing).
+Please send your pull request to the [testing branch](https://github.com/YunoHost-Apps/lxd_ynh/tree/testing).
 
 To try the testing branch, please proceed like that.
 ```
-sudo yunohost app install https://github.com/YunoHost-Apps/REPLACEBYYOURAPP_ynh/tree/testing --debug
+sudo yunohost app install https://github.com/YunoHost-Apps/lxd_ynh/tree/testing --debug
 or
-sudo yunohost app upgrade REPLACEBYYOURAPP -u https://github.com/YunoHost-Apps/REPLACEBYYOURAPP_ynh/tree/testing --debug
+sudo yunohost app upgrade lxd -u https://github.com/YunoHost-Apps/lxd_ynh/tree/testing --debug
 ```
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,15 +5,116 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+pkg_dependencies="acl autoconf dnsmasq-base git libacl1-dev libcap-dev liblxc1 lxc-dev libsqlite3-dev libtool libudev-dev libuv1-dev make pkg-config rsync squashfs-tools tar tcl xz-utils ebtables libapparmor-dev libseccomp-dev libcap-dev lvm2 thin-provisioning-tools btrfs-tools"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
 
+# Create a dedicated systemd socket config
+#
+# usage: ynh_add_systemd_config [--socket=socket] [--template=template]
+# usage: ynh_add_systemd_config [--socket=socket] [--template=template]  [--others_var="list of others variables to replace"]
+# | arg: -s, --socket=     - socket name (optionnal, $app by default)
+# | arg: -t, --template=    - Name of template file (optionnal, this is 'systemd' by default, meaning ./conf/systemd.socket will be used as template)
+# | arg: -v, --others_var=  - List of others variables to replace separated by a space. For example: 'var_1 var_2 ...'
+#
+# This will use the template ../conf/<templatename>.socket
+# to generate a systemd config, by replacing the following keywords
+# with global variables that should be defined before calling
+# this helper :
+#
+#   __APP__       by  $app
+#   __FINALPATH__ by  $final_path
+#
+# And dynamic variables (from the last example) :
+#   __VAR_1__    by $var_1
+#   __VAR_2__    by $var_2
+#
+ynh_add_systemd_socket_config () {
+    # Declare an array to define the options of this helper.
+    local legacy_args=stv
+    local -A args_array=( [s]=socket= [t]=template= [v]=others_var= )
+    local socket
+    local template
+    local others_var
+    # Manage arguments with getopts
+    ynh_handle_getopts_args "$@"
+    local socket="${socket:-$app}"
+    local template="${template:-systemd.socket}"
+    others_var="${others_var:-}"
+
+    finalsystemdconf="/etc/systemd/system/$socket.socket"
+    ynh_backup_if_checksum_is_different --file="$finalsystemdconf"
+    cp ../conf/$template "$finalsystemdconf"
+
+    # To avoid a break by set -u, use a void substitution ${var:-}. If the variable is not set, it's simply set with an empty variable.
+    # Substitute in a nginx config file only if the variable is not empty
+    if [ -n "${final_path:-}" ]; then
+        ynh_replace_string --match_string="__FINALPATH__" --replace_string="$final_path" --target_file="$finalsystemdconf"
+    fi
+    if [ -n "${app:-}" ]; then
+        ynh_replace_string --match_string="__APP__" --replace_string="$app" --target_file="$finalsystemdconf"
+    fi
+
+    # Replace all other variables given as arguments
+    for var_to_replace in $others_var
+    do
+        # ${var_to_replace^^} make the content of the variable on upper-cases
+        # ${!var_to_replace} get the content of the variable named $var_to_replace 
+        ynh_replace_string --match_string="__${var_to_replace^^}__" --replace_string="${!var_to_replace}" --target_file="$finalsystemdconf"
+    done
+
+    ynh_store_file_checksum --file="$finalsystemdconf"
+
+    chown root: "$finalsystemdconf"
+    systemctl enable "$socket.socket" --quiet
+    systemctl daemon-reload
+}
+
+# Remove the dedicated systemd socket config
+#
+# usage: ynh_remove_systemd_config [--socket=socket]
+# | arg: -s, --socket=     - socket name (optionnal, $app by default)
+#
+ynh_remove_systemd_socket_config () {
+    # Declare an array to define the options of this helper.
+    local legacy_args=s
+    local -A args_array=( [s]=socket= )
+    local socket
+    # Manage arguments with getopts
+    ynh_handle_getopts_args "$@"
+    local socket="${socket:-$app}"
+
+    local finalsystemdconf="/etc/systemd/system/$socket.socket"
+    if [ -e "$finalsystemdconf" ]
+    then
+        ynh_systemd_action --service_name="$socket.socket" --action=stop
+        systemctl disable $socket.socket --quiet
+        ynh_secure_remove --file="$finalsystemdconf"
+        systemctl daemon-reload
+    fi
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
+
+ynh_detect_arch(){
+	local architecture
+	if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
+		architecture="arm64"
+	elif [ -n "$(uname -m | grep 64)" ]; then
+		architecture="x86-64"
+	elif [ -n "$(uname -m | grep 86)" ]; then
+		architecture="i386"
+	elif [ -n "$(uname -m | grep arm)" ]; then
+		architecture="arm"
+	else
+		architecture="unknown"
+	fi
+	echo $architecture
+}
 
 #=================================================
 # FUTURE OFFICIAL HELPERS
